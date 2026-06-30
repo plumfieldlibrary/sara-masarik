4606145b53599a5cf28ba84d35a96ac4c518b048
michael-masarik <masarikfamilymichael@gmail.com>
2026-06-29 22:35:29 -0500

    Added nav bar and menu

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -1,8 +1,49 @@
+import { ListIcon } from "lucide-react";
 import type { ReactNode } from "react";
+import Link from "./Link";
+import { Button } from "./ui/button";
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger
+} from "./ui/navigation-menu";
 //TODO: Finish Nav Bar
 function Nav() {
     return (
-        <nav className="sticky top-0 left-0"></nav>
+        <nav className="sticky p-4 top-0 z-10 bg-foreground text-background shadow-sm flex items-center">
+            <div className="justify-start">
+                <span className="font-bold">Sara Masarik</span>
+            </div>
+            <div className="justify-end items-center ml-auto flex flex-row gap-4">
+
+                <NavigationMenu className="mr-auto">
+                    <NavigationMenuList>
+                        <NavigationMenuItem >
+                            <NavigationMenuTrigger>
+                                <ListIcon />
+                            </NavigationMenuTrigger>
+                            <NavigationMenuContent  >
+                                <NavigationMenuLink asChild>
+                                    <Link className="w-40" href="https://plumfieldmoms@gmail.com" utmContent="navbar">Blog</Link>
+                                </NavigationMenuLink>
+                                <NavigationMenuLink asChild >
+                                    <Link className="w-40" href="https://plumfieldpress.com" utmContent="navbar">Press</Link>
+                                </NavigationMenuLink>
+                                <NavigationMenuLink asChild >
+                                    <Link className="w-40" href="https://thecardcataloglibrarians.com" utmContent="navbar">The Card Catalog</Link>
+                                </NavigationMenuLink>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+                <Button className="bg-background text-foreground rounded-2xl hover:bg-background hover:text-background " asChild>
+                    <a href="https://plumfieldlibrary.com">Library</a>
+                </Button>
+            </div>
+        </nav>
     )
 }
 
