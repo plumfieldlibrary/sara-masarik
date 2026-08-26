6a98279b493b774f79f50eaaf432fa180518b1b1
michael-masarik <masarikfamilymichael@gmail.com>
2026-08-26 17:45:33 -0500

    Switched Navbar design and added favicons

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -1,49 +1,55 @@
 import { ListIcon } from "lucide-react";
 import type { ReactNode } from "react";
-import Link from "./Link";
 import { Button } from "./ui/button";
-import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger
-} from "./ui/navigation-menu";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+
+const NavLinks = [
+    {
+        title: "Library",
+        url: "https://plumfieldlibrary.com"
+    },
+    {
+        title: "Blog",
+        url: "https://plumfieldmoms.com"
+    },
+    {
+        title: "Press",
+        url: "https://plumfieldpress.com"
+    },
+    {
+        title: "The Card Catalog",
+        url: "https://thecardcataloglibrarians.com"
+    },
+    {
+        title: "Youtube",
+        url: "https://www.youtube.com/@PlumfieldLibrary"
+    }
+]
 //TODO: Finish Nav Bar
 function Nav() {
     return (
-        <nav className="sticky p-4 top-0 z-10 bg-foreground text-background shadow-sm flex items-center">
-            <div className="justify-start">
-                <span className="font-bold">Sara Masarik</span>
-            </div>
-            <div className="justify-end items-center ml-auto flex flex-row gap-4">
+        <Sheet>
+            <nav className="sticky p-4 top-0 z-10 bg-foreground text-background shadow-sm flex items-center">
+                <div className="justify-start">
+                    <span className="font-bold text-lg">Sara Masarik</span>
+                </div>
+                <div className="justify-end items-center ml-auto flex flex-row gap-4">
 
-                <NavigationMenu className="mr-auto">
-                    <NavigationMenuList>
-                        <NavigationMenuItem >
-                            <NavigationMenuTrigger>
-                                <ListIcon />
-                            </NavigationMenuTrigger>
-                            <NavigationMenuContent  >
-                                <NavigationMenuLink asChild>
-                                    <Link className="w-40" href="https://plumfieldmoms@gmail.com" utmContent="navbar">Blog</Link>
-                                </NavigationMenuLink>
-                                <NavigationMenuLink asChild >
-                                    <Link className="w-40" href="https://plumfieldpress.com" utmContent="navbar">Press</Link>
-                                </NavigationMenuLink>
-                                <NavigationMenuLink asChild >
-                                    <Link className="w-40" href="https://thecardcataloglibrarians.com" utmContent="navbar">The Card Catalog</Link>
-                                </NavigationMenuLink>
-                            </NavigationMenuContent>
-                        </NavigationMenuItem>
-                    </NavigationMenuList>
-                </NavigationMenu>
-                <Button className="bg-background text-foreground rounded-2xl hover:bg-background hover:text-background " asChild>
-                    <a href="https://plumfieldlibrary.com">Library</a>
-                </Button>
-            </div>
-        </nav>
+
+                    <SheetTrigger asChild><ListIcon /></SheetTrigger>
+                    <SheetContent showCloseButton={false} className="bg-background">
+                        <div className="text-center flex flex-col p-8 gap-4">
+                            {NavLinks.map((link) => (
+                                <a href={link.url} target="_blank" className="text-2xl text-foreground hover:text-white">{link.title}</a>
+                            ))}
+                        </div>
+                    </SheetContent>
+                    <Button size={"lg"} className="bg-background text-foreground rounded-2xl hover:bg-background hover:text-background " asChild>
+                        <a href="https://plumfieldlibrary.com">Library</a>
+                    </Button>
+                </div>
+            </nav>
+        </Sheet>
     )
 }
 
